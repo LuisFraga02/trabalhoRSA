05a63e6786d70ebaf44864ea80c2aa3828c73725
LuisFraga02 <luiseduardofragawermann@gmail.com>
2023-06-02 00:01:18 -0300

    out precisa exixtir no git :)

--- reset.bat
+++ reset.bat
@@ -6,4 +6,5 @@ echo +                                                              +
 echo +        todos os arquivos da pasta /out foram deletados       +
 echo +                                                              +
 echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-timeout /t 5 >nul
+timeout /t 3 >nul
+echo pasta out precisa existir para que o script funcione depois > out/out.txt
